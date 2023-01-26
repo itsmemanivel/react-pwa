98a6181ed6dcba4084093f18d465d7208fa0e879
veluvj <veluvijay1804@gmail.com>
2023-01-26 17:00:44 +0530

    feat: add filesystem Access API

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import PhotoCameraRoundedIcon from "@material-ui/icons/PhotoCameraRounded";
 import SaveIcon from '@material-ui/icons/Save';
+import {
+  fileSave,
+  supported,
+} from 'https://unpkg.com/browser-fs-access';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -26,7 +30,7 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
- function App() {
+function App() {
   const classes = useStyles();
   const [source, setSource] = useState("");
   const [img, setImg] = useState("");
@@ -40,12 +44,11 @@ const useStyles = makeStyles((theme) => ({
         };
         reader.readAsDataURL(target.files[0]);
         const newUrl = URL.createObjectURL(file);
-        console.log(newUrl);
         setSource(newUrl);
       }
     }
   };
- 
+
   function dataURItoBlob(dataURI) {
     const byteString = atob(dataURI.split(',')[1]);
     const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
@@ -54,28 +57,58 @@ const useStyles = makeStyles((theme) => ({
     for (let i = 0; i < byteString.length; i++) {
       ia[i] = byteString.charCodeAt(i);
     }
-    const blob = new Blob([ab], {type: mimeString});
+    const blob = new Blob([ab], { type: mimeString });
     return blob;
   }
 
-  const onSave = async ()=>{
-    const options = {
-      types: [
-        {
-          description: "Images",
-          accept: {
-            "image/png": [".png"],
-          },
-          suggestedName: "Capture"
-        },
-      ],
-    };
-   
-    const handle = await window.showSaveFilePicker(options);
-    const writable = await handle.createWritable();
-    await writable.write(dataURItoBlob(img.uploadedImage));
-    await writable.close();
-    return handle;
+  function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+  }
+
+  const onSave = async () => {
+
+    if (supported) {
+      console.log('Using the File System Access API.');
+    } else {
+      console.log('Using the fallback implementation.');
+    }
+
+    const blob = dataURItoBlob(img.uploadedImage);
+    const file = await fileSave(blob, {
+      fileName: document.title + "-" +makeid(5),
+      extensions: ['.png'],
+      description: 'PNG files',
+    });
+    if (file) {
+      document.title = file.name;
+      document.handle = file;
+      // updateContext(element);
+    }
+    // const options = {
+    //   types: [
+    //     {
+    //       description: "Images",
+    //       accept: {
+    //         "image/png": [".png"],
+    //       },
+    //       suggestedName: "Capture"
+    //     },
+    //   ],
+    // };
+
+    // const handle = await window.showSaveFilePicker(options);
+    // const writable = await handle.createWritable();
+    // await writable.write(dataURItoBlob(img.uploadedImage));
+    // await writable.close();
+    // return handle;
   };
 
   return (
