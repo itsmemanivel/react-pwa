10bdb4685f28ff21f0d7e24c1a63dba53c9d6bca
veluvj <veluvijay1804@gmail.com>
2023-01-26 16:13:02 +0530

    feat: add filesystem Access API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-// import About from "./About";
-// import Home from "./Home";
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import { IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import PhotoCameraRoundedIcon from "@material-ui/icons/PhotoCameraRounded";
+import SaveIcon from '@material-ui/icons/Save';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,48 +13,94 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
   },
   imgBox: {
-    maxWidth: "80%",
+    maxWidth: "100%",
     maxHeight: "80%",
     margin: "10px"
   },
   img: {
     height: "inherit",
-    maxWidth: "inherit",
+    maxWidth: "100%",
   },
   input: {
     display: "none"
   }
 }));
 
-function App() {
+ function App() {
   const classes = useStyles();
   const [source, setSource] = useState("");
+  const [img, setImg] = useState("");
   const handleCapture = (target) => {
-    if(target.files){
-      if(target.files.length !== 0){
+    if (target.files) {
+      if (target.files.length !== 0) {
         const file = target.files[0];
+        let reader = new FileReader();
+        reader.onload = function (e) {
+          setImg({ uploadedImage: e.target.result });
+        };
+        reader.readAsDataURL(target.files[0]);
         const newUrl = URL.createObjectURL(file);
+        console.log(newUrl);
         setSource(newUrl);
       }
     }
   };
+ 
+  function dataURItoBlob(dataURI) {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    const blob = new Blob([ab], {type: mimeString});
+    return blob;
+  }
+
+  const onSave = async ()=>{
+    const options = {
+      types: [
+        {
+          description: "Images",
+          accept: {
+            "image/png": [".png"],
+          },
+          suggestedName: "Capture"
+        },
+      ],
+    };
+   
+    const handle = await window.showSaveFilePicker(options);
+    const writable = await handle.createWritable();
+    await writable.write(dataURItoBlob(img.uploadedImage));
+    await writable.close();
+    return handle;
+  };
+
   return (
     <div className={classes.root}>
       <Grid container>
         <Grid item xs={12}>
           <h5>Capture your image</h5>
-          {source && 
+          {source &&
             <Box display="flex" justifyCenter="center" border={1} className={classes.imgBox}>
               <img src={source} alt={"snap"} className={classes.img}></img>
-            </Box>}
-            <input accept="image/*" className={classes.input} id='icon-button-file' type="file" capture="camera" onChange={(e) => handleCapture(e.target)} />
-            <label htmlFor='icon-button-file'>
-              <IconButton 
-                color="primary" aria-label="upload picture"
-                component="span">
-                  <PhotoCameraRoundedIcon fontSize="large" color="primary"/>
-                </IconButton>
-            </label>
+            </Box>
+          }
+          <input accept="image/*" className={classes.input} id='icon-button-file' type="file" capture="camera" onChange={(e) => handleCapture(e.target)} />
+          <label htmlFor='icon-button-file'>
+            <IconButton
+              color="primary" aria-label="upload picture"
+              component="span">
+              <PhotoCameraRoundedIcon fontSize="large" color="primary" />
+            </IconButton>
+          </label>
+          {source &&
+            <IconButton color="primary" aria-label="upload picture" component="span" onClick={() => onSave()}>
+              <SaveIcon fontSize="large" color="secondary" />
+            </IconButton>
+          }
         </Grid>
       </Grid>
     </div>
